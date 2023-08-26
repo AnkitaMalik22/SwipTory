18bbfe1ce63d28c826a61d1c610271d9a876ea7f
Ankita Malik <ankitamalik936@gmail.com>
2023-08-26 19:47:19 +0530

    removed port 4000

--- server/app.js
+++ server/app.js
@@ -70,7 +70,7 @@ app.get("*", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
